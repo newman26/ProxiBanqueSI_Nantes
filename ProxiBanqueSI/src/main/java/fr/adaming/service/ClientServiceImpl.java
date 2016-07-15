a50230f7bbe16b39830a47f1a05cf137c67c8fe8
nomane <nomane.boulmerdj@gmail.com>
2016-07-15 23:48:29 +0200

    Commit de la couche Service

---
+++ ProxiBanqueSI/src/main/java/fr/adaming/service/ClientServiceImpl.java
@@ -0,0 +1,51 @@
+package fr.adaming.service;
+
+import java.util.List;
+
+import fr.adaming.model.Client;
+
+public class ClientServiceImpl implements IClientService {
+
+	@Override
+	public void addClient(Client client) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateClient(Client client) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteClient(Client client) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Client> selectAllClients() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Client> selectClientByIdConseiller(Long idConseiller) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client findClientById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Client> findClientByNomAndPrenom(String nom, String prenom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ProxiBanqueSI/src/main/java/fr/adaming/service/ConseillerServiceImpl.java
@@ -0,0 +1,39 @@
+package fr.adaming.service;
+
+import java.util.List;
+
+import fr.adaming.model.Conseiller;
+
+public class ConseillerServiceImpl implements IConseillerService {
+
+	@Override
+	public void addConseiller(Conseiller conseiller) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateConseiller(Conseiller conseiller) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteConseiller(Conseiller conseiller) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Conseiller> selectAllConseillers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Conseiller findConseillerById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
