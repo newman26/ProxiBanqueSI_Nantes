758bf63107c60babf1fb0b0c87f3ae32a9d74a04
nomane <nomane.boulmerdj@gmail.com>
2016-07-18 13:14:59 +0200

    commit

--- ProxiBanqueSI/src/main/java/fr/adaming/dao/ConseillerDaoImpl.java
+++ ProxiBanqueSI/src/main/java/fr/adaming/dao/ConseillerDaoImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import fr.adaming.model.Client;
 import fr.adaming.model.Conseiller;
 
 
